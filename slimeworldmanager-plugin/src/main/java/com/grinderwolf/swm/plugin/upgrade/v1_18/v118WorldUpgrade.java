486bbef5d08c4a87555bcfbbd1c599554fdbcd92
Owen <23108066+Owen1212055@users.noreply.github.com>
2022-04-19 09:28:30 -0400

    Update v118WorldUpgrade.java

--- slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/upgrade/v1_18/v118WorldUpgrade.java
+++ slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/upgrade/v1_18/v118WorldUpgrade.java
@@ -327,7 +327,7 @@ public class v118WorldUpgrade implements Upgrade {
         CompoundMap map = new CompoundMap();
         CompoundTag tag = new CompoundTag("", map);
 
-        map.put("palette", palette);
+        map.put(new ListTag<>("palette", TagType.TAG_COMPOUND, palette));
         if (blockStates != null) {
             map.put(new LongArrayTag("data", blockStates));
         }
